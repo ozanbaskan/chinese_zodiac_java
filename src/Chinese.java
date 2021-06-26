8ff48eb8e8e0fe795d9337872f1b5a75ea7efea7
PC Principal <ozanbaskan7@gmail.com>
2021-06-27 01:28:53 +0300

    first commit

---
+++ src/Chinese.java
@@ -0,0 +1,57 @@
+import java.net.SocketOption;
+import java.util.Scanner;
+
+public class Chinese {
+    public static void main(String[] args) {
+        int birth_year;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Your birth year: ");
+        birth_year = s.nextInt();
+        if (birth_year < 0)
+        {
+            System.out.println("Invalid year");
+        }
+        else
+        {
+            switch(birth_year % 12)
+            {
+                case 0:
+                    System.out.println("Maymun");
+                    break;
+                case 1:
+                    System.out.println("Horoz");
+                break;
+                case 2:
+                    System.out.println("Köpek");
+                    break;
+                case 3:
+                    System.out.println("Domuz");
+                    break;
+                case 4:
+                    System.out.println("Fare");
+                    break;
+                case 5:
+                    System.out.println("Öküz");
+                    break;
+                case 6:
+                    System.out.println("Kaplan");
+                    break;
+                case 7:
+                    System.out.println("Tavşan");
+                    break;
+                case 8:
+                    System.out.println("Ejderha");
+                    break;
+                case 9:
+                    System.out.println("Yılan");
+                    break;
+                case 10:
+                    System.out.println("At");
+                    break;
+                case 11:
+                    System.out.println("Koyun");
+                    break;
+            }
+        }
+    }
+}
